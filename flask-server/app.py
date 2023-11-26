dcd4385ab6f085a2c81a1c2be322c5cfcfa00fbc
ninofelino12 <ninofelino12@gmail.com>
2023-11-26 20:02:49 +0700

    m2

--- flask-server/app.py
+++ flask-server/app.py
@@ -10,6 +10,10 @@ menu="""
       <a href="/" class="text-white py-2">Home</a>
       <a href="/odoo" class="text-white py-2">Odoo</a>
       <a href="/iot" class="text-white py-2">Iot</a>
+      <a href="/barcode" class="text-white py-2">Barcode</a>
+      <a href="/camera" class="text-white py-2">camera</a>
+      <a href="/iphone" class="text-white py-2">Iphone camera</a>
+      <a href="/viphone" class="text-white py-2">Iphone video</a>
       <a href="/docker" class="text-white py-2">Hosting</a>
 
 
@@ -27,6 +31,20 @@ models = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/object")
 @app.route('/')
 def hello():
     return render_template('index.html',menu=menu)
+
+@app.route('/barcode')
+def barcode():
+    return render_template('barcode.html',menu=menu)
+@app.route('/iphone')
+def iphone():
+    return render_template('iphonecamera.html',menu=menu)
+@app.route('/viphone')
+def viphone():
+    return render_template('video-iphone.html',menu=menu)
+
+@app.route('/camera')
+def camera():
+    return render_template('camera.html',menu=menu)
     
 
 @app.route('/odoo')
