cd02834c94249e05669d17911e9da818e4743a65
ninofelino12 <ninofelino12@gmail.com>
2023-11-29 14:26:04 +0700

    selasa

--- odoo15/addons/iot/controllers/controllers.py
+++ odoo15/addons/iot/controllers/controllers.py
@@ -5,9 +5,17 @@ from odoo import http
 class Iot(http.Controller):
     @http.route('/iot', auth='public')
     def index(self, **kw):
-        return http.request.render('iot.mobile',{})
+        my_string="""
+        Internet of Things (IoT) merujuk pada konsep di mana objek fisik di sekitar kita dilengkapi dengan teknologi dan terhubung ke internet. IoT memungkinkan objek tersebut untuk mengumpulkan dan bertukar data, serta mengambil tindakan berdasarkan data yang dikumpulkan. Ide dasar di balik IoT adalah membuat benda-benda sehari-hari menjadi pintar dan berkomunikasi satu sama lain, membentuk suatu ekosistem yang cerdas dan terhubung.
 
-    @http.route('/iot/iot/objects', auth='public')
+        """
+        return http.request.render('iot.mobile',{'my_string': my_string})
+    
+    @http.route('/barcode', auth='public')
+    def barcode(self, **kw):
+        return http.request.render('iot.barcode',{})
+
+    @http.route('/iot/objects', auth='public')
     def list(self, **kw):
         return http.request.render('iot.listing', {
             'root': '/iot/iot',
