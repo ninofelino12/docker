aa94c7bd5db32a07e0092b2b4a9794b5b3bb6eb6
ninofelino12 <ninofelino12@gmail.com>
2023-12-02 00:07:59 +0700

    jumat

---
+++ odoo15/addons/dev.sh
@@ -0,0 +1,9 @@
+    1  ls
+    2  cd /mnt
+    3  ls
+    4  cd extra-addons
+    5  ls
+    6  odoo scaffold barcode-scaner
+    7  odoo -u barcode-scanner -d felino
+    8  odoo scaffold barcodescaner
+    9  history >>dev.sh
